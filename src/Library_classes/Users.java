ca4f9a2bac9bd33923d78f0b87e938ba4a6e1e14
Bullet1395 <badmax666@list.ru>
2017-10-23 05:26:01 +0700

    Создал метод обработки пароля пользователя. Генерирует Salt + SHA512, получаем Hash пароля.

--- src/Library_classes/Users.java
+++ src/Library_classes/Users.java
@@ -1,13 +1,43 @@
 package Library_classes;
 
+import Security.EncryptedPass;
+
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+
 public class Users
 {
     private String Login;
     private String Password;
+    private String Salt;
 
-    public Users(String Login, String Password)
+    private static String SetSalt()
+    {
+        SecureRandom random = new SecureRandom();
+        byte bytes[] = new byte[16];
+        random.nextBytes(bytes);
+        return bytes.toString();
+    }
+
+    public Users(String Login, String Password) throws NoSuchAlgorithmException
     {
         this.Login = Login;
-        this.Password = Password;
+        this.Salt = SetSalt();
+        this.Password = EncryptedPass.hashPassword(Password, Salt);
+    }
+
+    public String GetLogin()
+    {
+        return this.Login;
+    }
+
+    public String GetPassword()
+    {
+        return this.Password;
+    }
+
+    public String GetSalt()
+    {
+        return this.Salt;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,16 @@
 import Library_classes.Users;
 
+import java.security.NoSuchAlgorithmException;
+
 public class Main {
 
-    public static void main(String[] args)
+    public static void main(String[] args) throws NoSuchAlgorithmException
     {
         Users User_Read = new Users("User_Read", "123_r");
-        Users User_Write = new Users ("User_Write", "123_w");
-        Users User_Execute = new Users ("User_Execute", "123_e");
+        Users User_Write = new Users("User_Write", "123_w");
+        Users User_Execute = new Users("User_Execute", "123_e");
 
-
-	    System.out.println();
+        System.out.println(User_Read.GetLogin() + " " + User_Read.GetSalt() + " " + User_Read.GetPassword() + '\n');
+        System.out.println(User_Write.GetLogin() + " " + User_Write.GetSalt() + " " + User_Write.GetPassword() + '\n');
     }
 }
