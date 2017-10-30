eaee9a207ce3a7279c4111f1abb42ad9f61845a9
Bullet1395 <badmax666@list.ru>
2017-10-30 15:58:04 +0700

    Review 10

--- src/Security/EncryptedPass.java
+++ src/Security/EncryptedPass.java
@@ -7,11 +7,18 @@ public class EncryptedPass {
     public static String hashPassword(String Password, String Salt) throws NoSuchAlgorithmException {
         StringBuffer hash = new StringBuffer();
         MessageDigest sha = MessageDigest.getInstance("SHA-512");
+        /*
+         * Метод возвращает hash пароля
+         * sha.update возвращает массив байтов для формирования MessageDigest в sha.digest
+         * в цикле занчения массива bytes переводим в стринг и соединяем в одну строку.
+         * 0xFF для коррктного отображения значений в диапазоне от 0..255 для 16-ричной системы счисления
+         * radix 16 - система счисления 16-ричная
+         */
         sha.update((Password + Salt).getBytes());
         byte[] bytes = sha.digest();
 
         for (int i = 0; i < bytes.length; i++) {
-            hash.append(Integer.toString((bytes[i] & 0xFF) + 0x100, 16).substring(1));
+            hash.append(Integer.toString((bytes[i] & 0xFF), 16));
         }
         return hash.toString();
     }
