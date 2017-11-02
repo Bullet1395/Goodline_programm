20c6c0004780e4248e5bf788d48b910ac63b8c35
Bullet1395 <badmax666@list.ru>
2017-11-02 20:53:37 +0700

    Добавил описание к методам. p19-20 [https://github.com/Bullet1395/Goodline_programm/issues/2]:

--- src/service/Authorization.java
+++ src/service/Authorization.java
@@ -7,6 +7,7 @@ import domain.enums.Roles;
 import java.util.List;
 
 public class Authorization {
+    //Проверяет параметры для авторизации
     public static void checkParam(Users user, List<Resources> resources, String role, String path) {
         int iter = 0;
         int iter2 = countInUser(resources, user.getLogin());
@@ -22,6 +23,21 @@ public class Authorization {
         }
     }
 
+    /*
+     * Проверяет, есть ли пользователь в таблице с ресурсами
+     * т.е. есть ли на его имя какие-то записи
+     */
+    private static int countInUser(List<Resources> resources, String user) {
+        int count_us = 0;
+        for (Resources res : resources) {
+            if (res.getUser().equals(user)) {
+                count_us++;
+            }
+        }
+        return count_us;
+    }
+
+    //Проверяет доступ к ресурсу
     private static boolean isCheckAccess(String role, String path, Resources res, int iter, int iter2){
         if (isCheckInRole(role)) {
             if (isCheckPathRole(path, res)) {
@@ -35,6 +51,7 @@ public class Authorization {
         return false;
     }
 
+    //Проверяет есть ли переданная из параметров роль в списке Enum ролей
     private static boolean isCheckInRole(String role) {
         for (Roles r : Roles.values()) {
             if (r.name().equals(role)) {
@@ -46,30 +63,30 @@ public class Authorization {
         return false;
     }
 
-    private static boolean isCheckRoleToResource(String role, Resources res, int iter, int iter2){
-        if (Roles.valueOf(role) == res.getRole()) {
-            return true;
-        } else if (iter == iter2) {
-            System.exit(4);
-        }
-        return false;
-    }
-
-    private static int countInUser(List<Resources> resources, String user) {
-        int count_us = 0;
-        for (Resources res : resources) {
-            if (res.getUser().equals(user)) {
-                count_us++;
-            }
-        }
-        return count_us;
-    }
-
+    /*
+     * Проверяет полученный путь ресурса из параметров(p1) с записью в таблице ресурсов(p2)
+     * Если количество символов p1 >= p2 тогда идет сравнение путей.
+     * у p2 остается столько символов сколько у p1, это позволяет реализоавть доступ ко всем внутренним папкам
+     * с такой же ролью. Проверка на роль была уже пройдена другим методом.
+     * т.е. есть запись в таблице C.R.RR, а передалась C.R.RR.T.G.H. т.к. у нас есть доступ к RR, а все остальные это вложенные
+     * ресурсы то к ним мы тоже получаем доступ.
+     * Если символов меньше, чем в записи таблицы, выходим из метода и проверяем следующую запись с таким именем пользователя
+     */
     private static boolean isCheckPathRole(String path, Resources resource) {
         if (path.length() >= resource.getPath().length()) {
             String p1 = resource.getPath();
             String p2 = path.substring(0, resource.getPath().length());
             return p1.equals((p2));
+        }
+        return false;
+    }
+
+    //Проверка роли ресурса
+    private static boolean isCheckRoleToResource(String role, Resources res, int iter, int iter2){
+        if (Roles.valueOf(role) == res.getRole()) {
+            return true;
+        } else if (iter == iter2) {
+            System.exit(4);
         }
         return false;
     }
