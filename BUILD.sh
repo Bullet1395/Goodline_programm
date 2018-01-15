98636e6b4fc7f0bc80c8b6e79291d4e1018b6f1b
Bullet1395 <bullet1395@gmail.com>
2018-01-15 13:04:46 +0700

    Подкорректировал файл BUILD.sh, добавил Roadmap3.md, исправил миграцию для создания RESOURCES и взаимодействеи ResourceDAO с бд, исправил файл pom.xml, добавил log4j2.xml

--- BUILD.sh
+++ BUILD.sh
@@ -2,7 +2,7 @@
 mkdir -p outHand
 
 # Сборка проектов в .class в папку outHand
-javac -d ./outHand/ -sourcepath ./src ./src/Main.java -classpath "./lib/commons-cli-1.4.jar:./lib/h2-1.4.196.jar:./lib/flyway-core-4.1.2.jar:./libs/log4j-core-2.8.2.jar:./lib/log4j-api-2.8.2.jar"
+javac -classpath "./src/main/resources/;./src/main/resources/db/migration;./lib/commons-cli-1.4.jar;./lib/h2-1.4.196.jar;./lib/flyway-core-5.0.5.jar;./lib/log4j-core-2.8.2.jar;./lib/log4j-api-2.8.2.jar" -encoding UTF-8 -d ./outHand/ -sourcepath ./src ./src/Main.java
 
 # Сборка проекта в .jar архив
 jar cvmf META-INF/MANIFEST.MF Goodline_programm.jar -C outHand .
