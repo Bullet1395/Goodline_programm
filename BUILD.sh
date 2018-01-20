fefe54335cc0c08422a6e2bfcca96a1e83f6467f
Bullet1395 <bullet1395@gmail.com>
2018-01-21 04:59:48 +0700

    Пробую исправить: Error: Could not find or load main class main.Main

--- BUILD.sh
+++ BUILD.sh
@@ -5,4 +5,4 @@ echo Сборка проектов .class в папку outHand
 javac -classpath "./src/main/resources/;./src/main/resources/db/migration;./lib/commons-cli-1.4.jar;./lib/h2-1.4.196.jar;./lib/flyway-core-5.0.5.jar;./lib/log4j-core-2.8.2.jar;./lib/log4j-api-2.8.2.jar" -encoding UTF-8 -d ./outHand/ -sourcepath ./src ./src/main/Main.java
 
 echo Сборка проекта в .jar архив
-jar cvmf META-INF/MANIFEST.MF Goodline_programm.jar -C outHand .
+jar cvmf META-INF/MANIFEST.MF Goodline_programm.jar outHand/ lib/
