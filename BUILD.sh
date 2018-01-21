83696589a0815abd91af67849404a4d791258416
Bullet1395 <bullet1395@gmail.com>
2018-01-21 18:15:26 +0700

    Пробую исправить: Error: Could not find or load main class main.Main

--- BUILD.sh
+++ BUILD.sh
@@ -1,13 +1,12 @@
 ﻿echo Создание папки для сборки проекта
-mkdir -p ./outHand
+mkdir -p outHand
 
 echo Сборка проектов .class в папку outHand
-#javac -cp "./lib/commons-cli-1.4.jar;./lib/h2-1.4.196.jar;./lib/flyway-core-5.0.5.jar;./lib/log4j-core-2.8.2.jar;./lib/log4j-api-2.8.2.jar" -d ./outHand/ -sourcepath ./src/main/java/ ./src/main/application/bin/Main.java
-javac -cp "./lib/commons-cli-1.4.jar:./lib/h2-1.4.196.jar:./lib/flyway-core-5.0.5.jar:./lib/log4j-core-2.8.2.jar:./lib/log4j-api-2.8.2.jar" -d ./outHand/ -sourcepath ./src/main/java/ ./src/main/application/bin/Main.java
+javac -cp "./lib/commons-cli-1.4.jar;./lib/h2-1.4.196.jar;./lib/flyway-core-5.0.5.jar;./lib/log4j-core-2.8.2.jar;./lib/log4j-api-2.8.2.jar" -d ./outHand/ -sourcepath src/main/java/ src/main/java/bin/Main.java
+#javac -cp "./lib/commons-cli-1.4.jar:./lib/h2-1.4.196.jar:./lib/flyway-core-5.0.5.jar:./lib/log4j-core-2.8.2.jar:./lib/log4j-api-2.8.2.jar" -d ./outHand/ -sourcepath ./src/main/java/ ./src/main/application/bin/Main.java
 
 echo Копируем ресурсы в outHand:
-mkdir -p ./outHand/resources
-cp -R -v ./src/main/resources/* ./outHand/resources/
+cp -R -v ./src/main/resources/* ./outHand/
 
 echo Сборка проекта в .jar архив
 jar cvmf META-INF/MANIFEST.MF Goodline_programm.jar outHand/ lib/
